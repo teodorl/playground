ad3c7f5c6e4c2438c3e7c545a9a92830e7c83163
Teodor Linder <teodorl@gmail.com>
2013-06-09 18:02:02 +0200

    Created RoR project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playground::Application.config.secret_token = '1fe4162823d37f6ef809f155c3a049291cbf9de03aff5af2a78f0e09c10c30cc7c53ed9a8736e6e7619f64da1d1b819dffbfc50c0bc9c20e81880587a030de77'
